e94166c180498ae5fe6fab0b1ae71b15852761df
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-07 22:28:33 +0300

    Update FloatTypeCasting.java

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-numberToBeRounded=19.6f;
 int x=Math.round(numberToBeRounded);
 System.out.println(x);
     }
